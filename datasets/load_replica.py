7a1ab386010565aa6e16d86dd45d39cc1f55db11
ChenLu-china <74493433+ChenLu-china@users.noreply.github.com>
2022-06-20 02:06:12 +0800

    Update load_replica.py

--- datasets/load_replica.py
+++ datasets/load_replica.py
@@ -38,6 +38,22 @@ def load_data(args):
     # test_ids = [x + step // 2 for x in train_ids]
     if args.editor:
         gt_img, gt_label, ori_pose, ins_rgbs, ins_num = processor(args.datadir, train_ids, test_ids, testskip=args.testskip).load_rgb()
+        
+        if view_id is not None:
+            view_poses = np.repeat(poses[view_id][np.newaxis, ...], args.views, axis=0)
+        else:
+            view_poses = torch.stack(
+                [pose_spherical(angle, -65.0, 7.0) for angle in np.linspace(-180, 180, args.views)], 0)
+        ins_num = len(ins_rgbs)
+        H, W = int(480), int(640)
+        focal = W / 2.0
+        K = np.array([[focal, 0, (W - 1) * 0.5], [0, focal, (H - 1) * 0.5], [0, 0, 1]])
+        hwk = [int(H), int(W), K]
+
+        # ins_img = labeltoimg(torch.LongTensor(gt_labels[40]), ins_rgbs)
+        # rgb8 = to8b(imgs[40])
+        # vis_segmentation(rgb8, ins_img)
+        return objs, view_poses, ins_map, pose, hwk, ins_rgbs, ins_num
     else:
         imgs, poses, i_split = rgb_processor(args.datadir, train_ids, test_ids, testskip=args.testskip).load_rgb()
         # add another load class which assigns to semantic labels
@@ -47,15 +63,15 @@ def load_data(args):
         gt_labels = ins_info.gt_labels
         ins_rgbs = ins_info.ins_rgbs
 
-    # ins_indices = ins_info.ins_indices
-    H, W = imgs[0].shape[:2]
-    # camera_angle_x = np.pi / 3.0
-    # focal = .5 * W / np.tan(.5 * camera_angle_x)
-    focal = W / 2.0
-    K = np.array([[focal, 0, (W - 1) * 0.5], [0, focal, (H - 1) * 0.5], [0, 0, 1]])
-    hwk = [int(H), int(W), K]
-    # ins_img = labeltoimg(torch.LongTensor(instances[80]), instances_colors)
-    # rgb8 = to8b(imgs[80])
-    # vis_segmentation(rgb8, ins_img)
+        # ins_indices = ins_info.ins_indices
+        H, W = imgs[0].shape[:2]
+        # camera_angle_x = np.pi / 3.0
+        # focal = .5 * W / np.tan(.5 * camera_angle_x)
+        focal = W / 2.0
+        K = np.array([[focal, 0, (W - 1) * 0.5], [0, focal, (H - 1) * 0.5], [0, 0, 1]])
+        hwk = [int(H), int(W), K]
+        # ins_img = labeltoimg(torch.LongTensor(instances[80]), instances_colors)
+        # rgb8 = to8b(imgs[80])
+        # vis_segmentation(rgb8, ins_img)
 
-    return imgs, poses, hwk, i_split, gt_labels, ins_rgbs, ins_info.ins_num
+        return imgs, poses, hwk, i_split, gt_labels, ins_rgbs, ins_info.ins_num
