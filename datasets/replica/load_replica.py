339d13c587d4e1d56f140a1e4b14ff6a0f8ce254
ChenLu-china <chen-lu.chen@connect.polyu.hk>
2022-08-23 15:01:04 +0800

    Update load_replica.py

--- datasets/replica/load_replica.py
+++ datasets/replica/load_replica.py
@@ -34,8 +34,7 @@ def load_data(args):
     total_num = 900
     step = 5
     train_ids = list(range(0, total_num, step))
-    test_ids = list(range(1, total_num, step))
-    # test_ids = [x + step // 2 for x in train_ids]
+    test_ids = [x + step // 2 for x in train_ids]
     if args.editor:
         objs, view_id,ins_map, poses, ins_rgbs = processor(args.datadir, train_ids, test_ids, testskip=args.testskip).load_rgb()
         
@@ -51,28 +50,18 @@ def load_data(args):
         K = np.array([[focal, 0, (W - 1) * 0.5], [0, focal, (H - 1) * 0.5], [0, 0, 1]])
         hwk = [int(H), int(W), K]
 
-        # ins_img = labeltoimg(torch.LongTensor(gt_labels[40]), ins_rgbs)
-        # rgb8 = to8b(imgs[40])
-        # vis_segmentation(rgb8, ins_img)
         return objs, view_poses, ins_map, poses, hwk, ins_rgbs, ins_num
     else:
         imgs, poses, i_split = rgb_processor(args.datadir, train_ids, test_ids, testskip=args.testskip).load_rgb()
-        # add another load class which assigns to semantic labels
 
         # load instance labels
         ins_info = ins_processor(args.datadir, train_ids, test_ids, None, None, testskip=args.testskip)
         gt_labels = ins_info.gt_labels
         ins_rgbs = ins_info.ins_rgbs
 
-        # ins_indices = ins_info.ins_indices
         H, W = imgs[0].shape[:2]
-        # camera_angle_x = np.pi / 3.0
-        # focal = .5 * W / np.tan(.5 * camera_angle_x)
         focal = W / 2.0
         K = np.array([[focal, 0, (W - 1) * 0.5], [0, focal, (H - 1) * 0.5], [0, 0, 1]])
         hwk = [int(H), int(W), K]
-        # ins_img = labeltoimg(torch.LongTensor(instances[80]), instances_colors)
-        # rgb8 = to8b(imgs[80])
-        # vis_segmentation(rgb8, ins_img)
 
         return imgs, poses, hwk, i_split, gt_labels, ins_rgbs, ins_info.ins_num
