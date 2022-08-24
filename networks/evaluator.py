6206f1f425a9fff363617909f67e06bf7eb1e715
ChenLu-china <chen-lu.chen@connect.polyu.hk>
2022-08-24 22:34:51 +0800

    Update evaluator.py

--- networks/evaluator.py
+++ networks/evaluator.py
@@ -6,14 +6,13 @@ from torch import nn
 from scipy.optimize import linear_sum_assignment
 
 bceloss = nn.BCELoss(reduction='none')
-thre_list = [0.5, 0.75, 0.8, 0.85, 0.9, 0.95]
-nllloss = nn.NLLLoss(ignore_index=-1, reduction='none')
-cross_entropy = nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
-img2mse = lambda x, y: torch.mean((x - y) ** 2)
 img2cross_entropy = nn.CrossEntropyLoss()
-mse2psnr = lambda x: -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+thre_list = [0.5, 0.75, 0.8, 0.85, 0.9, 0.95]
+img2mse = lambda x, y: torch.mean((x - y) ** 2)
+nllloss = nn.NLLLoss(ignore_index=-1, reduction='none')
 to8b = lambda x: (255 * np.clip(x, 0, 1)).astype(np.uint8)
-
+cross_entropy = nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
+mse2psnr = lambda x: -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
 
